f08b58e4247be09a5fab5478ae4cf18bad038b98
ChineseTravelGuide <97407209+ChineseTravelGuide@users.noreply.github.com>
2022-01-29 16:59:27 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,14 +2,14 @@ var i = 0;
 var images = [];
 var slideTime = 10000; // 10 seconds
 
-images[0] = 'images/GemsEN/zooEN.jpg';
-images[1] = 'images/GemsEN/botanicalEN.jpg';
-images[2] = 'images/GemsEN/bakkenEN.jpg';
-images[3] = 'images/GemsEN/cisternsEN.jpg';
-images[4] = 'images/GemsEN/planet.jpg';
-images[5] = 'images/GemsEN/rundetaarnEN.jpg';
-images[6] = 'images/GemsEN/smkEN.jpg';
-images[7] = 'images/GemsEN/zoologisk.jpg';
+images[0] = 'images/GemsEn/zooEN.jpg';
+images[1] = 'images/GemsEn/botanicalEN.jpg';
+images[2] = 'images/GemsEn/bakkenEN.jpg';
+images[3] = 'images/GemsEn/cisternsEN.jpg';
+images[4] = 'images/GemsEn/planet.jpg';
+images[5] = 'images/GemsEn/rundetaarnEN.jpg';
+images[6] = 'images/GemsEn/smkEN.jpg';
+images[7] = 'images/GemsEn/zoologisk.jpg';
 
 function changePicture() {
     document.body.style.backgroundImage = "url(" + images[i] + ")";
